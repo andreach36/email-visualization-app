ddfdc9544691c91b4c53b9308bc8cab141cd3ae6
andreach36 <andrea.haro36@gmail.com>
2025-01-13 23:46:05 -0500

    mobile responsive

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -30,26 +30,26 @@ onMounted(async () => {
 <template>
   <main class="overflow-hidden bg-gray-100 py-16 min-h-screen">
     <SearchBar></SearchBar>
-    <div class="grid grid-cols-2 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <!-- Tabla -->
       <div class="flex flex-col px-6">
         <table class="table-fixed w-full border-collapse border border-gray-300">
           <thead>
             <tr class="bg-violet-300">
-              <th class="border border-gray-300 px-4 py-2 text-xs font-medium text-gray-700">Subject</th>
-              <th class="border border-gray-300 px-4 py-2 text-xs font-medium text-gray-700">From</th>
-              <th class="border border-gray-300 px-4 py-2 text-xs font-medium text-gray-700">To</th>
+              <th class="border border-gray-300 px-1 md:px-4 py-2 text-xs font-medium text-gray-700">Subject</th>
+              <th class="border border-gray-300 px-1 md:px-4 py-2 text-xs font-medium text-gray-700">From</th>
+              <th class="border border-gray-300 px-1 md:px-4 py-2 text-xs font-medium text-gray-700">To</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="datarow in emailStore.dataEmails" :key="datarow.Id" class="bg-white">
-              <td class="cursor-pointer border border-gray-300 px-4 py-2 text-center text-xs truncate text-ellipsis whitespace-nowrap" @click="showBody(datarow)">
+              <td class="cursor-pointer border border-gray-300 px-1 md:px-4 py-2 text-center text-xs truncate text-ellipsis whitespace-nowrap" @click="showBody(datarow)">
                 {{ datarow.Subject }}
               </td>
-              <td class="cursor-pointer border border-gray-300 px-4 py-2 text-center text-xs truncate text-ellipsis whitespace-nowrap" @click="showBody(datarow)">
+              <td class="cursor-pointer border border-gray-300 px-1 md:px-4 py-2 text-center text-xs truncate text-ellipsis whitespace-nowrap" @click="showBody(datarow)">
                 {{ datarow.From }}
               </td>
-              <td class="cursor-pointer border border-gray-300 px-4 py-2 text-center text-xs truncate text-ellipsis whitespace-nowrap" @click="showBody(datarow)">
+              <td class="cursor-pointer border border-gray-300 px-1 md:px-4 py-2 text-center text-xs truncate text-ellipsis whitespace-nowrap" @click="showBody(datarow)">
                 {{ datarow.To }}
               </td>
             </tr>
@@ -59,17 +59,17 @@ onMounted(async () => {
           <button class="px-2 py-1 bg-sky-800 text-white text-sm rounded disabled:opacity-50"
           :disabled="emailStore.dataPagination.page_num <= 1"
           @click="changePage(emailStore.dataPagination.page_num - 1)">Before</button>
-          <span class="text-sm  text-gray-700">Page {{ emailStore.dataPagination.page_num }} from {{ emailStore.dataPagination.total_pages }}</span>
+          <span class="text-sm text-center text-gray-700">Page {{ emailStore.dataPagination.page_num }} from {{ emailStore.dataPagination.total_pages }}</span>
           <button class="px-2 py-1 bg-sky-800 text-white text-sm rounded disabled:opacity-50"
           :disabled="emailStore.dataPagination.page_num >= emailStore.dataPagination.total_pages"
           @click="changePage(emailStore.dataPagination.page_num + 1)">Next</button>
-          <span class="text-sm  text-gray-700">Total Data: {{ emailStore.dataPagination.total_data }} emails</span>
+          <span class="text-sm text-center text-gray-700">Total Data: {{ emailStore.dataPagination.total_data }} emails</span>
         </div>
       </div>
       <!-- Detalles del email -->
-      <div class="bg-white px-6 py-4 mr-8 rounded-lg h-[70vh] overflow-auto">
+      <div class="bg-white px-6 py-4 m-4 md:m-0 md:mr-8 rounded-lg h-[70vh] overflow-auto">
         <h2 class="font-bold text-lg">{{ emailStore.emailVisualization.Subject }}</h2>
-        <p class="tracking-normal leading-relaxed text-justify text-sm py-3">{{ emailStore.emailVisualization.Body }}</p>
+        <p class="tracking-normal leading-relaxed text-justify text-sm py-3 overflow-hidden">{{ emailStore.emailVisualization.Body }}</p>
       </div>
     </div>
   </main>
