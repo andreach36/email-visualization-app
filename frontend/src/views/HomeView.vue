87d4022d2b468faa87cdcfd7d6b21dc4077fdade
andreach36 <andrea.haro36@gmail.com>
2025-01-15 23:57:14 -0500

    email information modal

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,14 +2,21 @@
 import SearchBar from '@/components/SearchBar.vue';
 import { useEmailStore } from '@/stores/data';
 import type { Email } from '@/types';
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const emailStore = useEmailStore()
+const isModalOpen = ref(false);
 
 // Mostrar contenido del email
 function showBody(emailData: Email){
   emailStore.emailVisualization.Subject = emailData.Subject
   emailStore.emailVisualization.Body = emailData.Body
+  isModalOpen.value = true;
+}
+
+// Cerrar el modal
+function closeModal() {
+  isModalOpen.value = false;
 }
 
 // Cambiar página
@@ -66,12 +73,22 @@ onMounted(async () => {
           <span class="text-sm text-center text-gray-700">Total Data: {{ emailStore.dataPagination.total_data }} emails</span>
         </div>
       </div>
-      <!-- Detalles del email -->
-      <div class="bg-white px-6 py-4 m-4 md:m-0 md:mr-8 rounded-lg h-[70vh] overflow-auto">
+      <!-- Detalles del email (Desktop) -->
+      <div class="hidden md:block bg-white px-6 py-4 m-4 md:m-0 md:mr-8 rounded-lg h-[70vh] overflow-auto">
         <h2 class="font-bold text-lg">{{ emailStore.emailVisualization.Subject }}</h2>
         <p class="tracking-normal leading-relaxed text-justify text-sm py-3 overflow-hidden">{{ emailStore.emailVisualization.Body }}</p>
       </div>
     </div>
+
+    <!-- Modal (Mobile) -->
+    <div v-if="isModalOpen" class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 md:hidden">
+      <div class="bg-white rounded-lg p-4 max-w-sm w-full max-h-[80vh] overflow-auto relative">
+        <button @click="closeModal" class="absolute top-2 right-2 bg-red-700 rounded text-white text-2xl font-bold px-1">&times;</button>
+        <h2 class="font-bold text-lg">{{ emailStore.emailVisualization.Subject }}</h2>
+        <p class="tracking-normal leading-relaxed text-justify text-sm py-3">{{ emailStore.emailVisualization.Body }}</p>
+      </div>
+    </div>
+
   </main>
 </template>
 
